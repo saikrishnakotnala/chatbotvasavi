f11ee3ecfe434e0f15e65bf203aac276432f6e53
Sai krishna kotnala <saikrishnachinnu8@gmail.com>
2018-11-16 02:22:54 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  dialogflow: {
+    Acumen: '10e5d7d612a140df947a8b6da097c530'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  dialogflow: {
+    Acumen: '10e5d7d612a140df947a8b6da097c530'
+  }
+};
